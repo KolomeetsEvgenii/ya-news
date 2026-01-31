e95a22fe4d4876f324a31ec41d60f00ab91ed321
Evgenii Kolomeets <kolomeetsevgenii@yandex.ru>
2026-01-31 20:42:04 +1000

    ver2

---
+++ news/tests/test_content.py
@@ -0,0 +1,73 @@
+# flake8: noqa
+from datetime import datetime, timedelta
+
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+from django.conf import settings
+from django.test import TestCase
+from django.urls import reverse
+
+from news.models import Comment, News
+from news.forms import CommentForm
+
+User = get_user_model()
+
+class TestHomePage(TestCase):
+    HOME_URL = reverse('news:home')
+
+    @classmethod
+    def setUpTestData(cls):
+        today = datetime.today()
+        News.objects.bulk_create(
+            News(
+                title=f'Новость {index}',
+                text='Просто текст.',
+                date=today - timedelta(days=index)
+            )
+            for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
+    )
+
+    def test_news_count(self):
+        response = self.client.get(self.HOME_URL)
+        object_list = response.context['object_list']
+        news_count = object_list.count()
+        self.assertEqual(news_count, settings.NEWS_COUNT_ON_HOME_PAGE)
+
+    def test_news_order(self):
+        response = self.client.get(self.HOME_URL)
+        object_list = response.context['object_list']
+        all_dates = [news.date for news in object_list]
+        sorted_dates = sorted(all_dates, reverse=True)
+        self.assertEqual(all_dates, sorted_dates)
+
+class TestDetailPage(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(title='Тестовая новость', text='Просто текст.')
+        cls.detail_url = reverse('news:detail', args=(cls.news.id,))
+        cls.author = User.objects.create(username='Комментатор')
+        now = timezone.now()
+        for index in range(10):
+            comment = Comment(news=cls.news, author=cls.author, text=f'Tекст {index}')
+        comment.created = now + timedelta(days=index)
+        comment.save()
+
+    def test_comments_order(self):
+        response = self.client.get(self.detail_url)
+        self.assertIn('news', response.context)
+        news = response.context['news']
+        all_comments = news.comment_set.all()
+        all_timestamps = [comment.created for comment in all_comments]
+        sorted_timestamps = sorted(all_timestamps)
+        self.assertEqual(all_timestamps, sorted_timestamps)
+
+    def test_anonymous_client_has_no_form(self):
+        responce = self.client.get(self.detail_url)
+        self.assertNotIn('form', responce.context)
+
+    def test_authorized_client_has_form(self):
+        self.client.force_login(self.author)
+        responce = self.client.get(self.detail_url)
+        self.assertIn('form', responce.context)
+        self.assertIsInstance(responce.context['form'], CommentForm)
